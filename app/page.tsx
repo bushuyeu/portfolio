462c4ff33ba2bfef3b3cf36c218c921f30bdedb1
Pavel Bushuyeu <pavelbushuyeu@gmail.com>
2024-08-12 15:37:13 -0400

    playing with copy a bit more

--- app/page.tsx
+++ app/page.tsx
@@ -10,31 +10,23 @@ export default function Page() {
         {`Hello. My name is Pavel and I am a technology product leader and entrepreneur with a passion for product design.
         `}
       </h3>
-      <h4 className="mb-4 text-l">
+      <h4 className="mb-4 text-xl">
         {`
-        Selected achievements and awards
+        Selected achievements and awards:
         `}
       </h4>
       <ul>
         <li className="mb-4">
-          {`
-        Wealth Advisor: SmartX named as the fastest growing Asset Management Platform, 2022
-        `}
+          {`- Wealth Advisor named SmartX the fastest growing Asset Management Platform of 2022;`}
         </li>
         <li className="mb-4">
-          {`
-        Gartner: LogicMonitor named as the Best Monitoring Tools, 2019
-        `}
+          {`- Gartner named LogicMonitor the Best Monitoring Tools of 2019;`}
         </li>
         <li className="mb-4">
-          {`
-        Facebook: Klondike gets Facebooks's Best Web Geme, 2015
-        `}
+          {`— Facebook names Vizor's Knights & Brides the Best New Game of 2016;`}
         </li>
         <li className="mb-4">
-          {`
-        Facebook: Knights & Brides - Facebook's Best New Game, 2016
-        `}
+          {`— Facebook names Vizor's Klondike the Best Web Geme of 2015.`}
         </li>
       </ul>
 
