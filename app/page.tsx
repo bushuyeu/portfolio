39d2a978ca105268bdb013c8ebe04c1672175d76
Pavel Bushuyeu <pavelbushuyeu@gmail.com>
2024-08-12 15:04:23 -0400

    edits

--- app/page.tsx
+++ app/page.tsx
@@ -6,25 +6,15 @@ export default function Page() {
       <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
         Pavel Bushuyeu
       </h1>
-      <p className="mb-4">
+      <h3 className="mb-5">
         {`Hello. My name is Pavel and I am a technology product leader and entrepreneur with a passion for product design.
-        
-        Selected achievements and awards:
-        ‍ - SmartX
-            fastest growing Asset Management Platform, 2022
-            Beta launch of a billing application, 2021
-        - LogicMonitor
-            Gartner's Best Monitoring Tools, 2019
-        - Gurtam
-            Re-launch of mobile apps, 2017
-            Mobile logins 5% -> 75% of total platform logins in 1 year, 2018
-        - Vizor Games
-            Klondike - Facebooks's Best Web Geme, 2015
-            Knights & Brides - Facebook's Best New Game, 2016
-
-        Outside of tech, I enjoys endurance sports and wild nature.
         `}
-      </p>
+      </h3>
+      <h4 className="mb-4">
+        {`
+        Selected achievements and awards:
+        `}
+      </h4>
       <div className="my-8">
         <BlogPosts />
       </div>
