c3983343f456d1715e53377b3e9cf5296a629013
Pavel Bushuyeu <pavelbushuyeu@gmail.com>
2024-08-12 15:20:48 -0400

    email added

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -19,7 +19,7 @@ export default function Footer() {
   return (
     <footer className="mb-16">
       <ul className="font-sm mt-8 flex flex-col space-x-0 space-y-2 text-neutral-600 md:flex-row md:space-x-4 md:space-y-0 dark:text-neutral-300">
-        <li>
+        {/* <li>
           <a
             className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
             rel="noopener noreferrer"
@@ -28,6 +28,17 @@ export default function Footer() {
           >
             <ArrowIcon />
             <p className="ml-2 h-7">rss</p>
+          </a>
+        </li> */}
+        <li>
+          <a
+            className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
+            rel="noopener noreferrer"
+            target="_blank"
+            href="mailto:p@bushuyeu.com"
+          >
+            <ArrowIcon />
+            <p className="ml-2 h-7">email</p>
           </a>
         </li>
         <li>
