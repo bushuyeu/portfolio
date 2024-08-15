eeb02109f04b1fb04cba5cb899109439f7ba2b26
Pavel Bushuyeu <pavelbushuyeu@gmail.com>
2024-08-15 00:26:06 -0400

    front page copy

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,9 +1,10 @@
-import { BlogPosts } from 'app/components/posts'
+import { BlogPosts } from "app/components/posts";
 
 export const metadata = {
-  title: 'Blog',
-  description: 'Read my blog.',
-}
+  title: "PB chatting",
+  description:
+    "Pavel Bushuyeu posts on his path from product management to entrepreneurship",
+};
 
 export default function Page() {
   return (
@@ -11,5 +12,5 @@ export default function Page() {
       <h1 className="font-semibold text-2xl mb-8 tracking-tighter">My Blog</h1>
       <BlogPosts />
     </section>
-  )
+  );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -8,40 +8,89 @@ export default function Page() {
       </h1>
       <h3 className="mb-4">
         {` 
-        Hello. I'm Pavel, a technology product leader, focused on B2B SaaS solutions.
+        Hello. I'm Pavel, an entrepreneur and technology product leader, focused on B2B SaaS solutions. American University in Bulgaria & 11 Ventures start-up program alum. 
         `}
       </h3>
       <h3 className="mb-4">
         {` 
-        Currently, I'm working on launching products in AI space, mentoring other product managers and advising start-ups on product strategy.
+        Currently, I'm focused on launching AI products, mentoring product managers, and assisting startups with product strategy and UX architecture and design.
         `}
       </h3>
-      <h4 className="mb-4 text-xl">
-        {`
-        Selected awards
+      <h3 className="mb-4 text-xl">
+        {` 
+        Previous positions
         `}
-      </h4>
-      <ul>
-        <li className="mb-4">
-          {`- Wealth Advisor named SmartX the fastest growing Asset Management Platform of 2022;`}
+      </h3>
+      <ul className="list-disc ml-4">
+        <li className="mb-2">
+          Sr Product Manager @{" "}
+          <a
+            href="https://www.smartxadvisory.com/"
+            className="text-blue-400 underline"
+          >
+            SmartX
+          </a>
+          , asset management platform;
         </li>
-        <li className="mb-4">
-          {`- Gartner named LogicMonitor the Best Monitoring Tools of 2019;`}
+        <li className="mb-2">
+          Product Manager @{" "}
+          <a
+            href="https://logicmonitor.com/"
+            className="text-blue-400 underline"
+          >
+            LogicMonitor
+          </a>
+          , infrastructure monitoring platform;
         </li>
-        <li className="mb-4">
-          {`- Facebook names Vizor's Knights & Brides the Best New Game of 2016;`}
+        <li className="mb-2">
+          Product Manager @{" "}
+          <a href="https://een.com/" className="text-blue-400 underline">
+            EEN
+          </a>
+          , video management system;
         </li>
-        <li className="mb-4">
-          {`- Facebook names Vizor's Klondike the Best Web Geme of 2015.`}
+        <li className="mb-2">
+          Product Manager @{" "}
+          <a href="https://gurtam.com/" className="text-blue-400 underline">
+            Gurtam
+          </a>
+          , fleet management system;
+        </li>
+        <li className="mb-8">
+          Associate Product Manager @{" "}
+          <a
+            href="https://vizor-games.com/"
+            className="text-blue-400 underline"
+          >
+            Vizor
+          </a>
+          , game development studio.
         </li>
       </ul>
-      <br></br>
-      <p className="mb-4">
-        {`Besides tech, I also enjoy endurance sports and wild nature.`}
-      </p>
-      {/* <div className="my-8">
+      <h4 className="mb-4 text-xl">
+        {`
+        Selected Awards
+        `}
+      </h4>
+      <ul className="list-disc ml-4">
+        <li className="mb-2">
+          {`Wealth Advisor named SmartX the fastest growing Asset Management Platform of 2022;`}
+        </li>
+        <li className="mb-2">
+          {`Gartner named LogicMonitor the Best Monitoring Tools of 2019;`}
+        </li>
+        <li className="mb-2">
+          {`Facebook named Vizor's Knights & Brides the Best New Game of 2016;`}
+        </li>
+        <li className="mb-8">
+          {`Facebook named Vizor's Klondike the Best Web Geme of 2015.`}
+        </li>
+      </ul>
+
+      <p className="mb-2">{`Recent blog posts:`}</p>
+      <div className="my-8">
         <BlogPosts />
-      </div> */}
+      </div>
     </section>
   );
 }
