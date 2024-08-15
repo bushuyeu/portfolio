29dd9f2ebde53285aef418ce3f33a6beda9a06d3
Pavel Bushuyeu <pavelbushuyeu@gmail.com>
2024-08-15 00:45:22 -0400

    copy edits

--- app/page.tsx
+++ app/page.tsx
@@ -8,12 +8,12 @@ export default function Page() {
       </h1>
       <h3 className="mb-4">
         {` 
-        Hello. I'm Pavel, an entrepreneur and technology product leader, focused on B2B SaaS solutions. American University in Bulgaria & 11 Ventures start-up program alum. 
+        Hello, I'm Pavel, an entrepreneur and technology product leader specializing in B2B SaaS solutions. I am an alum of the American University in Bulgaria and the 11 Ventures start-up program.
         `}
       </h3>
       <h3 className="mb-4">
         {` 
-        Currently, I'm focused on launching AI products, mentoring product managers, and assisting startups with product strategy and UX architecture and design.
+        Currently, I am focused on launching AI products, mentoring product managers, and assisting startups with product strategy, UX architecture, and design.
         `}
       </h3>
       <h3 className="mb-4 text-xl">
@@ -30,7 +30,7 @@ export default function Page() {
           >
             SmartX
           </a>
-          , asset management platform;
+          , an asset management platform;
         </li>
         <li className="mb-2">
           Product Manager @{" "}
@@ -40,21 +40,21 @@ export default function Page() {
           >
             LogicMonitor
           </a>
-          , infrastructure monitoring platform;
+          , an infrastructure monitoring platform;
         </li>
         <li className="mb-2">
           Product Manager @{" "}
           <a href="https://een.com/" className="text-blue-400 underline">
             EEN
           </a>
-          , video management system;
+          , a video management system;
         </li>
         <li className="mb-2">
           Product Manager @{" "}
           <a href="https://gurtam.com/" className="text-blue-400 underline">
             Gurtam
           </a>
-          , fleet management system;
+          , a fleet management system;
         </li>
         <li className="mb-8">
           Associate Product Manager @{" "}
@@ -64,7 +64,7 @@ export default function Page() {
           >
             Vizor
           </a>
-          , game development studio.
+          , a game development studio.
         </li>
       </ul>
       <h4 className="mb-4 text-xl">
@@ -74,16 +74,16 @@ export default function Page() {
       </h4>
       <ul className="list-disc ml-4">
         <li className="mb-2">
-          {`Wealth Advisor named SmartX the fastest growing Asset Management Platform of 2022;`}
+          {`Wealth Advisor named SmartX the Fastest Growing Asset Management Platform of 2022;`}
         </li>
         <li className="mb-2">
-          {`Gartner named LogicMonitor the Best Monitoring Tools of 2019;`}
+          {`Gartner named LogicMonitor as the Best Monitoring Tools of 2019;`}
         </li>
         <li className="mb-2">
           {`Facebook named Vizor's Knights & Brides the Best New Game of 2016;`}
         </li>
         <li className="mb-8">
-          {`Facebook named Vizor's Klondike the Best Web Geme of 2015.`}
+          {`Facebook named Vizor's Klondike the Best Web Game of 2015.`}
         </li>
       </ul>
       {/* 
