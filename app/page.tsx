34cad0ae18032bdadcae3b4c7cf66b0bbdbf8790
Pavel Bushuyeu <pavelbushuyeu@gmail.com>
2025-05-02 14:06:25 -0700

    copy

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ export default function Page() {
         Pasha Bushuyeu
       </h1>
       <h3 className="mb-4">
-        {`My mission is to empower people to become healthier, wealthier, and more useful. I do this by building tech products and providing support.`}
+        {`My mission is to empower people to become healthier, wealthier, and more useful. I do this by building tech products and providing support & mentorship.`}
       </h3>
       <h3 className="mb-4 text-xl">{`My specialties:`}</h3>
       <ul className="list-disc ml-4">
