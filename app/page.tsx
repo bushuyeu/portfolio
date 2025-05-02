cdd6c2605a87cc09bb4239a5aed85ecc2b72c591
Pavel Bushuyeu <pavelbushuyeu@gmail.com>
2025-05-02 13:46:14 -0700

    copy edits

--- app/page.tsx
+++ app/page.tsx
@@ -8,26 +8,26 @@ export default function Page() {
         Pasha Bushuyeu
       </h1>
       <h3 className="mb-4">
-        {`My mission is to empower people to become healthier, wealthier, and more useful. I do this by building tech products and providing support.`}
+        {`My mission is to empower people to become healthier, wealthier, and more useful. I do this by building tech products.`}
       </h3>
       <h3 className="mb-4 text-xl">{`My specialties:`}</h3>
       <ul className="list-disc ml-4">
         <li className="mb-2">
           0 to 1: I’ve helped launch several profitable products, including
-          Video Dokuta - video AI for physical world, SmartX’s Billing,
-          LogicMonitor’s UX monitoring, and EEN’s Body Camera App.
+          Video Dokuta — a video AI for the physical world; billing at SmartX;
+          UX Monitoring at LogicMonitor; Body Camera App at EEN.
         </li>
         <li className="mb-2">
-          ML Product: I enjoy building smart products utlizing machine learning.
-          Examples: Root Cause Analysis of infrastructure alerts for
-          LogicMonitor Topology, driver's score for Gurtam Wialon, GenAI
-          utlization at Video Dokuta.
+          ML Products: I enjoy building data products powered by machine
+          learning. Examples: root cause analysis of infrastructure alerts for
+          LogicMonitor Topology, driver scoring for Gurtam Wialon, and GenAI
+          utilization at Video Dokuta.
         </li>
 
         <li className="mb-2">
-          Design: I advocate for strong design and utilization of front-end and
-          design libraries to reduce time to market. I’ve helped built design
-          systems for SmartX and LogicMonitor.
+          Design: I advocate for strong design principles and the use of
+          front-end to design libraries to accelerate design and development.
+          I’ve helped build design systems at both SmartX and LogicMonitor.
         </li>
       </ul>
       <h3 className="mb-4 text-xl">
