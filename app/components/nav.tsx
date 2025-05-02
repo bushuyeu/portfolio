e8e80404c53c2854c1fa96d1425e0c992ce9d31c
Pavel Bushuyeu <pavelbushuyeu@gmail.com>
2025-05-02 14:16:47 -0700

    remove resume

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -10,10 +10,6 @@ const navItems = {
   "/blog": {
     name: "blog", // blog
   },
-  "https://docs.google.com/document/d/12Yf3TqCObbycOHVQyLlXvIoimgqcZ57mUNnfKVAQ3ao/edit?usp=sharing":
-    {
-      name: "resume", // resume
-    },
 };
 
 export function Navbar() {
