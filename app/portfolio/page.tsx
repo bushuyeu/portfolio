4c9f0949b4e7e20e6bfb8136d59bb1d132a0b9ab
Pavel Bushuyeu <pavelbushuyeu@gmail.com>
2025-05-02 12:33:43 -0700

    copy

--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -2,7 +2,7 @@ import { PortfolioWorks } from "app/components/works";
 
 export const metadata = {
   title: "Portfolio - PB",
-  description: "Pasha Bushuyeu's sample works",
+  description: "Pasha Bushuyeu's work samples",
 };
 
 export default function Page() {
