bd37847f8c48feebc5c5cfd4d5db628be82f8279
Pavel Bushuyeu <pavelbushuyeu@gmail.com>
2025-05-02 19:44:18 -0700

    header padding

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -14,27 +14,24 @@ const navItems = {
 
 export function Navbar() {
   return (
-    <aside className="mb-16 tracking-tight">
-      <div className="lg:sticky lg:top-20">
-        <nav
-          className="flex flex-row items-start relative px-0 pb-0 fade md:overflow-auto scroll-pr-6 md:relative"
-          id="nav"
-        >
-          <div className="flex flex-row space-x-0 pr-10">
+    <nav
+      id="nav"
+      className="mb-16 tracking-tight"
+    >
+      <ul className="font-sm flex flex-col space-x-0 space-y-2 text-neutral-600 md:flex-row md:space-x-4 md:space-y-0 dark:text-neutral-300">
             {Object.entries(navItems).map(([path, { name }]) => {
               return (
-                <Link
-                  key={path}
-                  href={path}
-                  className="transition-all hover:text-neutral-800 dark:hover:text-neutral-200 flex align-middle relative py-1 px-2 m-1"
-                >
-                  {name}
-                </Link>
+                <li key={path}>
+                  <Link
+                    href={path}
+                    className="transition-all hover:text-neutral-800 dark:hover:text-neutral-200 flex align-middle relative"
+                  >
+                    {name}
+                  </Link>
+                </li>
               );
             })}
-          </div>
-        </nav>
-      </div>
-    </aside>
+      </ul>
+    </nav>
   );
 }
